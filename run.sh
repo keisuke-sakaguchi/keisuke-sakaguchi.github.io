830c37dc31be34ce939799a1c519c8d247437cb6
keisks <keisks5@gmail.com>
2019-11-25 11:19:19 -0800

    update

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+export PATH=/usr/local/opt/ruby/bin:$PATH
+gem install jekyll bundler
+bundle install
 bundle exec jekyll serve
