5f63f9d6af68743c8594302f2dd25930a06b2a10
keisks <keisks5@gmail.com>
2021-04-04 10:35:35 -0700

    up

--- Gemfile
+++ Gemfile
@@ -8,3 +8,5 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 # gem "rails"
 
 #gem "jekyll", "~> 4.0"
+
+gem "webrick", "~> 1.7"
--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+rm ./Gemfile.lock
+
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 export PATH=/usr/local/opt/ruby/bin:$PATH
 gem install jekyll bundler
 bundle install
+bundle add webrick
 bundle exec jekyll serve
